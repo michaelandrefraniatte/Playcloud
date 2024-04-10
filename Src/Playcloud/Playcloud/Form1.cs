aa2e5c91328879cd85fedee9dc31720ca6f122ee
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-11 00:59:47 +0200

    Initial commit

---
+++ Src/Playcloud/Playcloud/Form1.cs
@@ -0,0 +1,347 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using WebView2 = Microsoft.Web.WebView2.WinForms.WebView2;
+using System.Drawing;
+using System.ComponentModel;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Playcloud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("USER32.DLL")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+        [DllImport("user32.dll")]
+        static extern bool DrawMenuBar(IntPtr hWnd);
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        public static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        public static uint CurrentResolution = 0;
+        private static string WINDOW_NAME = "";
+        private const int GWL_STYLE = -16;
+        private const uint WS_BORDER = 0x00800000;
+        private const uint WS_CAPTION = 0x00C00000;
+        private const uint WS_SYSMENU = 0x00080000;
+        private const uint WS_MINIMIZEBOX = 0x00020000;
+        private const uint WS_MAXIMIZEBOX = 0x00010000;
+        private const uint WS_OVERLAPPED = 0x00000000;
+        private const uint WS_POPUP = 0x80000000;
+        private const uint WS_TABSTOP = 0x00010000;
+        private const uint WS_VISIBLE = 0x10000000;
+        private static int width, height;
+        private static bool f11switch = false;
+        public WebView2 webView21 = new WebView2();
+        public static bool echoboostenable = false;
+        private static int x, y, cx, cy;
+        public static int vkCode, scanCode;
+        public static bool KeyboardHookButtonDown, KeyboardHookButtonUp;
+        public static bool starting = true;
+        public static int[] wd = { 2, 2, 2, 2 };
+        public static int[] wu = { 2, 2, 2, 2 };
+        public static void valchanged(int n, bool val)
+        {
+            if (val)
+            {
+                if (wd[n] <= 1)
+                {
+                    wd[n] = wd[n] + 1;
+                }
+                wu[n] = 0;
+            }
+            else
+            {
+                if (wu[n] <= 1)
+                {
+                    wu[n] = wu[n] + 1;
+                }
+                wd[n] = 0;
+            }
+        }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution);
+            x = 0;
+            y = 0;
+            cx = Screen.PrimaryScreen.Bounds.Width;
+            cy = Screen.PrimaryScreen.Bounds.Height;
+            this.Size = new Size(cx, cy);
+            this.Location = new Point(x, y);
+            this.label1.Location = new Point(cx / 2 - this.label1.Size.Width / 2, cy / 2 - this.label1.Height / 2 - this.label2.Height);
+            this.label2.Location = new Point(cx / 2 - this.label2.Size.Width / 2, cy / 2 - this.label2.Height / 2 + this.label2.Height);
+            this.pictureBox1.Location = new Point(cx / 2 - this.pictureBox1.Size.Width / 2, cy * 1 / 4);
+            this.progressBar1.Location = new Point(cx / 2 - this.progressBar1.Size.Width / 2, cy * 2 / 3);
+            Task.Run(() => Loader());
+            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-gpu --disable-gpu-compositing", "en");
+            CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
+            await webView21.EnsureCoreWebView2Async(environment);
+            webView21.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
+            webView21.CoreWebView2.WebResourceRequested += CoreWebView2_WebResourceRequested;
+            webView21.CoreWebView2.Settings.AreDevToolsEnabled = true;
+            webView21.CoreWebView2.ContainsFullScreenElementChanged += (obj, args) =>
+            {
+                this.FullScreen = webView21.CoreWebView2.ContainsFullScreenElement;
+            };
+            using (StreamReader file = new StreamReader("playcloud.txt"))
+            {
+                webView21.Source = new Uri(file.ReadLine());
+            }
+            webView21.Dock = DockStyle.Fill;
+            webView21.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            webView21.CoreWebView2.ContextMenuRequested += CoreWebView2_ContextMenuRequested;
+            webView21.NavigationCompleted += WebView21_NavigationCompleted;
+            webView21.KeyDown += WebView21_KeyDown;
+            webView21.DefaultBackgroundColor = Color.Black;
+            this.Controls.Add(webView21);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            OnKeyDown(keyData);
+            return true;
+        }
+        private void WebView21_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        private void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (keyData == Keys.F11)
+            {
+                if (!f11switch)
+                {
+                    width = Screen.PrimaryScreen.Bounds.Width;
+                    height = Screen.PrimaryScreen.Bounds.Height;
+                    WINDOW_NAME = GetActiveWindowTitle();
+                    if (WINDOW_NAME == "Playcloud")
+                    {
+                        IntPtr window = FindWindowByCaption(IntPtr.Zero, WINDOW_NAME);
+                        SetWindowLong(window, GWL_STYLE, WS_SYSMENU);
+                        SetWindowPos(window, -2, 0, 0, width, height, 0x0040);
+                        DrawMenuBar(window);
+                        f11switch = true;
+                    }
+                }
+                else
+                {
+                    WINDOW_NAME = GetActiveWindowTitle();
+                    if (WINDOW_NAME == "Playcloud")
+                    {
+                        IntPtr window = FindWindowByCaption(IntPtr.Zero, WINDOW_NAME);
+                        SetWindowLong(window, GWL_STYLE, WS_CAPTION | WS_POPUP | WS_BORDER | WS_SYSMENU | WS_TABSTOP | WS_VISIBLE | WS_OVERLAPPED | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+                        SetWindowPos(window, -2, x, y, cx, cy, 0x0040);
+                        DrawMenuBar(window);
+                        f11switch = false;
+                    }
+                }
+            }
+        }
+        private void Loader()
+        {
+            while (this.progressBar1.Value <= 100)
+            {
+                this.progressBar1.Value++;
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (starting)
+            {
+                this.label1.Location = new Point(this.Width / 2 - this.label1.Size.Width / 2, this.Height / 2 - this.label1.Height / 2 - this.label2.Height);
+                this.label2.Location = new Point(this.Width / 2 - this.label2.Size.Width / 2, this.Height / 2 - this.label2.Height / 2 + this.label2.Height);
+                this.pictureBox1.Location = new Point(this.Width / 2 - this.pictureBox1.Size.Width / 2, this.Height * 1 / 4);
+                this.progressBar1.Location = new Point(this.Width / 2 - this.progressBar1.Size.Width / 2, this.Height * 2 / 3);
+            }
+        }
+        private void WebView21_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (starting)
+            {
+                starting = false;
+                this.Controls.Remove(progressBar1);
+                this.Controls.Remove(label1);
+                this.Controls.Remove(label2);
+                this.Controls.Remove(label3);
+                this.Controls.Remove(pictureBox1);
+            }
+        }
+        private void CoreWebView2_WebResourceRequested(object sender, CoreWebView2WebResourceRequestedEventArgs e)
+        {
+            CoreWebView2HttpRequestHeaders requestHeaders = e.Request.Headers;
+            requestHeaders.SetHeader("Access-Control-Max-Age", "0");
+            requestHeaders.SetHeader("Cache-Control", "max-age=0, public");
+            requestHeaders.SetHeader("Access-Control-Allow-Origin", "*");
+            requestHeaders.SetHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            requestHeaders.SetHeader("Access-Control-Allow-Headers", "x-requested-with, content-type");
+        }
+        private void CoreWebView2_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            e.Handled = true;
+            webView21.Source = new System.Uri(e.Uri);
+        }
+        private void CoreWebView2_ContextMenuRequested(object sender, CoreWebView2ContextMenuRequestedEventArgs e)
+        {
+            IList<CoreWebView2ContextMenuItem> menuList = e.MenuItems;
+            CoreWebView2ContextMenuItem newItem;
+            newItem = webView21.CoreWebView2.Environment.CreateContextMenuItem("Copy page Uri", null, CoreWebView2ContextMenuItemKind.Command);
+            newItem.CustomItemSelected += delegate (object send, Object ex)
+            {
+                string pageUri = e.ContextMenuTarget.PageUri;
+                System.Threading.SynchronizationContext.Current.Post((_) =>
+                {
+                    Clipboard.SetText(pageUri);
+                    System.Diagnostics.Process.Start(pageUri);
+                }, null);
+            };
+            menuList.Insert(menuList.Count, newItem);
+            newItem = webView21.CoreWebView2.Environment.CreateContextMenuItem("Go to menu", null, CoreWebView2ContextMenuItemKind.Command);
+            newItem.CustomItemSelected += delegate (object send, Object ex)
+            {
+                System.Threading.SynchronizationContext.Current.Post((_) =>
+                {
+                    using (StreamReader file = new StreamReader("playcloud.txt"))
+                    {
+                        webView21.Source = new Uri(file.ReadLine());
+                    }
+                }, null);
+            };
+            menuList.Insert(menuList.Count, newItem);
+            newItem = webView21.CoreWebView2.Environment.CreateContextMenuItem("Go to Uri", null, CoreWebView2ContextMenuItemKind.Command);
+            newItem.CustomItemSelected += delegate (object send, Object ex)
+            {
+                string newpageUri = Microsoft.VisualBasic.Interaction.InputBox("Prompt", "Enter a new page Uri", "https://google.com", 0, 0);
+                System.Threading.SynchronizationContext.Current.Post((_) =>
+                {
+                    if (newpageUri != "")
+                        Navigate(newpageUri);
+                }, null);
+            };
+            menuList.Insert(menuList.Count, newItem);
+            newItem = webView21.CoreWebView2.Environment.CreateContextMenuItem("Back", null, CoreWebView2ContextMenuItemKind.Command);
+            newItem.CustomItemSelected += delegate (object send, Object ex)
+            {
+                System.Threading.SynchronizationContext.Current.Post((_) =>
+                {
+                    string stringinject = @"
+                        history.back();
+                    ".Replace("\r\n", " ");
+                    execScriptHelper(stringinject);
+                }, null);
+            };
+            menuList.Insert(menuList.Count, newItem);
+            newItem = webView21.CoreWebView2.Environment.CreateContextMenuItem("Forward", null, CoreWebView2ContextMenuItemKind.Command);
+            newItem.CustomItemSelected += delegate (object send, Object ex)
+            {
+                System.Threading.SynchronizationContext.Current.Post((_) =>
+                {
+                    string stringinject = @"
+                        history.forward();
+                    ".Replace("\r\n", " ");
+                    execScriptHelper(stringinject);
+                }, null);
+            };
+            menuList.Insert(menuList.Count, newItem);
+            newItem = webView21.CoreWebView2.Environment.CreateContextMenuItem("Reload", null, CoreWebView2ContextMenuItemKind.Command);
+            newItem.CustomItemSelected += delegate (object send, Object ex)
+            {
+                System.Threading.SynchronizationContext.Current.Post((_) =>
+                {
+                    string stringinject = @"
+                        window.location.reload(false);
+                    ".Replace("\r\n", " ");
+                    execScriptHelper(stringinject);
+                }, null);
+            };
+            menuList.Insert(menuList.Count, newItem);
+        }
+        private void Navigate(string address)
+        {
+            if (String.IsNullOrEmpty(address))
+                return;
+            if (address.Equals("about:blank"))
+                return;
+            if (!address.StartsWith("http://") & !address.StartsWith("https://"))
+                address = "https://" + address;
+            try
+            {
+                webView21.Source = new System.Uri(address);
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+        }
+        private bool fullScreen = false;
+        [DefaultValue(false)]
+        public bool FullScreen
+        {
+            get { return fullScreen; }
+            set
+            {
+                fullScreen = value;
+                if (value)
+                {
+                    this.WindowState = FormWindowState.Normal;
+                    FormBorderStyle = FormBorderStyle.None;
+                    WindowState = FormWindowState.Maximized;
+                }
+                else
+                {
+                    this.Activate();
+                    this.FormBorderStyle = FormBorderStyle.Sizable;
+                    this.WindowState = FormWindowState.Normal;
+                }
+            }
+        }
+        private static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+        private async Task<String> execScriptHelper(String script)
+        {
+            var x = await webView21.ExecuteScriptAsync(script).ConfigureAwait(false);
+            return x;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            webView21.Dispose();
+        }
+    }
+}
